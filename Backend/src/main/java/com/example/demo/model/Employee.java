1c8c174944fc96391ab69e8fbb31800af534f00f
Fernando TPR <it21835278@my.sliit.lk>
2024-02-03 22:55:59 +0530

    frontend created

--- Backend/src/main/java/com/example/demo/model/Employee.java
+++ Backend/src/main/java/com/example/demo/model/Employee.java
@@ -1,5 +1,83 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="employees")
 public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy =GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="address")
+	private String address;
+	
+	@Column(name="emailId")
+	private String emailId;
+	
+	
+	public Employee() {
+	}
+
+
+	public Employee(long id, String name, String address, String emailId) {
+		this.id = id;
+		this.name = name;
+		this.address = address;
+		this.emailId = emailId;
+	}
+
+
+	public long getId() {
+		return id;
+	}
+
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	
+	
+	
+	
 
 }
